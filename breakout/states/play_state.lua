7ad06f300efa961d34cc2693782fc1c80cad08e2
Carlos Camilo <carloscamilocontato@gmail.com>
2024-12-27 15:09:40 -0300

    breakout 5

--- breakout/states/play_state.lua
+++ breakout/states/play_state.lua
@@ -46,13 +46,48 @@ function PlayState:update(dt)
 	self.ball:update(dt)
 
 	if self.ball:collides(self.paddle) then
+		self.ball.y = self.paddle.y - self.ball.height
 		self.ball.dy = -self.ball.dy
+
+		if self.ball.x < self.paddle.x + self.paddle.width / 2 and self.paddle.dx < 0 then
+			self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
+		elseif self.ball.x > self.paddle.x + self.paddle.width / 2 and self.paddle.dx > 0 then
+			self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+		end
+
 		G.SOUNDS.PADDLE_HIT:play()
 	end
 
 	for _, brick in pairs(self.bricks) do
 		if brick.in_scene and self.ball:collides(brick) then
 			brick:hit()
+
+			-- Left corner collision
+			if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+				self.ball.dx = -self.ball.dx
+				self.ball.x = brick.x - self.ball.height
+
+			-- Right corner collision
+			elseif self.ball.x + 5 < brick.x + brick.width and self.ball.dx < 0 then
+				self.ball.dx = -self.ball.dx
+				self.ball.x = brick.x + brick.width
+
+			-- Top collision
+			elseif self.ball.y < brick.y then
+				self.ball.dy = -self.ball.dy
+				self.ball.y = brick.y - self.ball.height
+
+			-- Bottom collision
+			else
+				self.ball.dy = -self.ball.dy
+				self.ball.y = brick.y + brick.height
+			end
+
+			-- Slowly increase game speed
+			self.ball.dy = self.ball.dy * 1.02
+
+			-- Prevent colliding with more than one brick
+			break
 		end
 	end
 
